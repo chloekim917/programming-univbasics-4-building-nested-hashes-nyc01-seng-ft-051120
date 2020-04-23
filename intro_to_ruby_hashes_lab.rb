435a9347720f77cae478c7574e730b5f6bcd564e
Seoyoung Kim <mintholic1@gmail.com>
2020-04-23 19:39:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,10 @@
 def base_hash
-  ahash = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
+  ahash = {:railroads => {}
 
 end
 
 def monopoly_with_second_tier
-   ahash = {:railroads => {}}
+   ahash = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
